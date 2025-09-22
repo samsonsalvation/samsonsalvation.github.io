8a9e84d8122b5fbac0bbf265edfd158870dce090
kenyyode <samsonsalvation@gmail.com>
2025-09-22 17:49:03 +0100

    Google analytics basic installation

--- src/Sections/MorePosts.jsx
+++ src/Sections/MorePosts.jsx
@@ -7,7 +7,7 @@ const MorePosts = (props) => {
         <section className="px-8 sm:max-w-3xl mx-auto items-left text-left justify-center mt-12 sm:mt-20 tracking-tight space-y-8"> 
             <div className=""> 
                 <h2 className="font-medium text-2xl sm:text-4xl mb-10 sm:mb-8"> {props.title} </h2>
-                <div className="flex flex-col sm:flex-row  sm:gap-12 h-full w-auto"> 
+                <div className="flex flex-col sm:flex-row gap-12 h-full w-auto"> 
                     {Previews.map((Preview, index) => (
                         <div key={index} className="flex-1 text-left">
                             <Link to={Preview.url}>
