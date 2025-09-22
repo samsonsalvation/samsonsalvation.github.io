996cfd997d6cbf0660446e8c2d668488c46f97c3
kenyyode <samsonsalvation@gmail.com>
2025-09-22 10:29:49 +0100

    Updated basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/portfolio2025'>
+    <BrowserRouter basename='/'>
        <App />
     </BrowserRouter>
   </StrictMode>,
