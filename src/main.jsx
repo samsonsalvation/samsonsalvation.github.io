7d576b033b138eba4c0d7590563f51cf009f07a2
kenyyode <samsonsalvation@gmail.com>
2025-09-22 10:47:10 +0100

    Updating basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/samsonsalvation.github.io">
+    <BrowserRouter>
        <App />
     </BrowserRouter>
   </StrictMode>,
